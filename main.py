32420c10ec8c940ccdf22e9711bbe706b4fa4730
Alfie <bwqk3256@leeds.ac.uk>
2026-03-18 21:10:35 +0000

    Set up

---
+++ main.py
@@ -0,0 +1,10 @@
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
+import mutagen.id3
+from mutagen.id3 import ID3, TIT2, TIT3, TALB, TPE1, TRCK, TYER
+
+"""
+Ref:
+https://methodmatters.github.io/editing-id3-tags-mp3-meta-data-in-python/
+"""
+
