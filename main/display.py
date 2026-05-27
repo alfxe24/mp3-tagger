cca1ff8114afdd66ff17707b3e9f9624a812f3a3
Alfie <bwqk3256@leeds.ac.uk>
2026-05-27 18:12:29 +0100

    Created Tkinter Menu for Directory Treeview

---
+++ main/display.py
@@ -0,0 +1,56 @@
+import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename, askdirectory
+
+
+class Window:
+    def __init__(self, master):
+        self.tree = None
+        self.rootDir = None
+        self.master = master
+        self.ask_for_directory()
+
+    def ask_for_directory(self):
+        tk.Label(self.master, text="Welcome to MP3 Tagger!", font=("TkDefaultFont", 20)).grid(row=0, column=0, padx=10, pady=5)
+        tk.Label(self.master, text="Please select a root directory.", font=("TkDefaultFont", 15)).grid(row=1, column=0, padx=10, pady=5)
+        self.rootDir = askdirectory()
+        print(self.rootDir)
+        self.show_treeview()
+
+    def show_treeview(self):
+        """
+        Ref:
+        https://stackoverflow.com/questions/16746387/display-directory-content-with-tkinter-treeview-widget
+        """
+        clear_window(self.master)
+        self.tree = ttk.Treeview()
+        ysb = ttk.Scrollbar(self.master, orient='vertical', command=self.tree.yview)
+        xsb = ttk.Scrollbar(self.master, orient='horizontal', command=self.tree.xview)
+        self.tree.configure(yscroll=ysb.set, xscroll=xsb.set)
+        self.tree.heading('#0', text=self.rootDir, anchor='w')
+
+        abspath = os.path.abspath(self.rootDir)
+        root_node = self.tree.insert('', 'end', text=abspath, open=True)
+        self.process_directory(root_node, abspath)
+
+        self.tree.grid(row=0, column=0)
+        ysb.grid(row=0, column=1, sticky='ns')
+        xsb.grid(row=1, column=0, sticky='ew')
+
+    def process_directory(self, parent, path):
+        for p in os.listdir(path):
+            abspath = os.path.join(path, p)
+            isdir = os.path.isdir(abspath)
+            oid = self.tree.insert(parent, 'end', text=p, open=False)
+            if isdir:
+                self.process_directory(oid, abspath)
+
+def clear_window(master):
+    for element in master.winfo_children():
+        element.destroy()
+
+def init():
+    root = tk.Tk()
+    window = Window(root)
+    root.mainloop()
---
+++ main/main.py
@@ -0,0 +1,21 @@
+# Import Packages
+"""from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
+import mutagen.id3
+from mutagen.id3 import ID3, TIT2, TIT3, TALB, TPE1, TRCK, TYER"""
+
+
+# Import my files
+import display
+
+"""
+Ref:
+https://methodmatters.github.io/editing-id3-tags-mp3-meta-data-in-python/
+"""
+
+def main():
+    display.init()
+
+if __name__ == "__main__":
+    main()
+
